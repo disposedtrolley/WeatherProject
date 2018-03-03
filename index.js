52feef69529a6ad6dfc768e1c467d9224727249d
jamesliu <jamesliu@au1.ibm.com>
2018-03-03 11:35:33 +1100

    add input field and text element

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
-import App from './App';
+import WeatherProject from './WeatherProject';
 
-AppRegistry.registerComponent('WeatherProject', () => App);
+AppRegistry.registerComponent('WeatherProject', () => WeatherProject);
